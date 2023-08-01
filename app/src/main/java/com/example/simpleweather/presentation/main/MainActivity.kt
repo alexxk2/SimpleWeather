93b2622066a24885bd94d5aa13a0b05a4718f1bb
alexxk2 <alexx.koda@gmail.com>
2023-08-01 23:38:33 +0300

    Commit #1. Добавление зависимостей

--- app/src/main/java/com/example/simpleweather/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/simpleweather/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.simpleweather
+package com.example.simpleweather.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.simpleweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
