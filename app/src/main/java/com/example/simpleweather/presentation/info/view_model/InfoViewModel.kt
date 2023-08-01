c4e45ed18087e9c830085e9bdc3c3e6c7d22e01d
alexxk2 <alexx.koda@gmail.com>
2023-08-02 00:40:36 +0300

    Commit #2. Добавлены фрагменты и layout для поиска

---
+++ app/src/main/java/com/example/simpleweather/presentation/info/view_model/InfoViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.simpleweather.presentation.info.view_model
+
+import androidx.lifecycle.ViewModel
+
+class InfoViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/example/simpleweather/presentation/search/view_model/SearchViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.simpleweather.presentation.search.view_model
+
+import androidx.lifecycle.ViewModel
+
+class SearchViewModel:ViewModel() {
+}
